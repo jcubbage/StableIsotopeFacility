03ff23b17659e59a40d211199ffdada6b6d843b0
James Cubbage <jscubbage@ucdavis.edu>
2020-10-14 16:55:47 -0700

    shipping address

--- Areas/Client/Controllers/AddressController.cs
+++ Areas/Client/Controllers/AddressController.cs
@@ -20,13 +20,80 @@ namespace SIFCore.Controllers
         }
 
         public async Task<IActionResult> Index()
+        {
+            var contacts = await _dbContext.Contacts.Where(c => c.Id == int.Parse(User.FindFirstValue("contactId")))
+                .Include(c => c.BillingAddresseses)
+                .Include(c => c.ShippingAddresseses)
+                .FirstAsync();
+            return View(contacts);
+        }
+
+        public async Task<IActionResult> ShippingDetails(int id)
+        {
+            var address = await _dbContext.ShippingAddresses.Where(a => a.Id == id && a.Contact.Id == int.Parse(User.FindFirstValue("contactId"))).FirstOrDefaultAsync();
+            if(address == null)
             {
-                var contacts = await _dbContext.Contacts.Where(c => c.Id == int.Parse(User.FindFirstValue("contactId")))
-                    .Include(c => c.BillingAddresseses)
-                    .Include(c => c.ShippingAddresseses)
-                    .FirstAsync();
-                return View(contacts);
+                ErrorMessage = "Address not found or you do not have access to it";
+                return RedirectToAction(nameof(Index)); 
             }
+            return View(address);
+        }
+
+        public async Task<IActionResult> ShippingEdit(int id)
+        {
+            var address = await _dbContext.ShippingAddresses.Where(a => a.Id == id && a.Contact.Id == int.Parse(User.FindFirstValue("contactId"))).FirstOrDefaultAsync();
+            if(address == null)
+            {
+                ErrorMessage = "Address not found or you do not have access to it";
+                return RedirectToAction(nameof(Index)); 
+            }
+            return View(address);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ShippingEdit(int id, ShippingAddresses address)
+        {
+            var addressToUpdate = await _dbContext.ShippingAddresses.Where(a => a.Id == id && a.Contact.Id == int.Parse(User.FindFirstValue("contactId"))).FirstOrDefaultAsync();
+            if(address == null)
+            {
+                ErrorMessage = "Address not found or you do not have access to it";
+                return RedirectToAction(nameof(Index)); 
+            }
+            addressToUpdate.AddressName = address.AddressName;
+            addressToUpdate.Institution = address.Institution;
+            addressToUpdate.Department = address.Department;
+            addressToUpdate.PIFirstName = address.PIFirstName;
+            addressToUpdate.PILastName = address.PILastName;
+            addressToUpdate.PIEmail = address.PIEmail;
+            addressToUpdate.PIEmailAdditional = address.PIEmailAdditional;
+            addressToUpdate.PIFax = address.PIFax;
+            addressToUpdate.PIPhone = address.PIPhone;
+            addressToUpdate.Address1 = address.Address1;
+            addressToUpdate.Address2 = address.Address2;
+            addressToUpdate.Address3 = address.Address3;
+            addressToUpdate.City = address.City;
+            addressToUpdate.State = address.State;
+            addressToUpdate.ZipCode = address.ZipCode;
+            addressToUpdate.Country = address.Country;
+
+             if(ModelState.IsValid){
+                await _dbContext.SaveChangesAsync();
+                Message = "Shipping Address updated";
+            } else {
+                ErrorMessage = "Something went wrong";         
+                return View(address);
+            }
+
+            return RedirectToAction(nameof(ShippingDetails), new { id = id }); 
+        }
+        
+         public IActionResult ShippingCreate()
+        {
+            var address = new ShippingAddresses(); 
+            address.Country = "U.S.A.";          
+            return View(address);
+        }
+
     }
 
 }

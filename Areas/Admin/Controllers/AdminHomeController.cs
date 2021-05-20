5a6a7c97b0a13c1fd4ae84b094426af4a5c32cc7
James Cubbage <jscubbage@ucdavis.edu>
2021-05-19 20:26:40 -0700

    Admin landing page

---
+++ Areas/Admin/Controllers/AdminHomeController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SIFCore.Models;
+
+namespace SIFCore.Controllers
+{
+    public class AdminHomeController : AdminController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}

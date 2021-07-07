18bef330f36ed9e979419988da35a0ad28238c1f
James Cubbage <jscubbage@ucdavis.edu>
2021-07-06 20:12:40 -0700

    Handle charges

---
+++ Areas/Admin/Controllers/ChargesController.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SIFCore.Helpers;
+using SIFCore.Models;
+
+namespace SIFCore.Controllers.Admin
+{    
+    public class ChargesController : AdminController
+    {
+         private readonly SIFContext _dbContext;
+
+        public ChargesController(SIFContext dbContext)
+        {            
+            _dbContext = dbContext;
+        }
+
+        
+        public async Task<IActionResult> Edit(int id)
+        {
+           var model = await _dbContext.Charges.Where(c => c.Id == id).FirstOrDefaultAsync();
+
+            if(model == null)
+            {
+                ErrorMessage = "Charge not found";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+         [HttpPost]
+         public async Task<IActionResult> Edit(int id, Charges charge)
+         {
+            var chargeToUpdate = await _dbContext.Charges.Where(r => r.Id == id).FirstOrDefaultAsync();
+            
+            
+            if(chargeToUpdate == null || chargeToUpdate.Id != charge.Id)
+            {
+                ErrorMessage = "Charge not found or doesn't match call";
+                return RedirectToAction(nameof(OrdersController.Index),"Orders");
+            }
+            chargeToUpdate.Description = charge.Description;
+            chargeToUpdate.ItemCount = charge.ItemCount;
+            chargeToUpdate.Cost = charge.Cost;
+            chargeToUpdate.Paid = charge.Paid;              
+
+            if(ModelState.IsValid){                 
+                await _dbContext.SaveChangesAsync();
+                Message = "Charge Updated";
+            } else {
+                ErrorMessage = "Something went wrong"; 
+                var model = await _dbContext.Charges.Where(c => c.Id == id).FirstOrDefaultAsync();
+                return View(model);
+            }           
+            
+            return RedirectToAction(nameof(OrdersController.Details),"Orders", new {  id = chargeToUpdate.OrderId});
+            
+         }    
+
+         public async Task<IActionResult> Charge(int id)    
+         {
+            var analysis = await _dbContext.Analysis.Include(a => a.AnalysisRequirement).Where(a => a.OrderId == id).ToListAsync();
+            var order = await _dbContext.Orders.Where(o => o.Id == id).FirstOrDefaultAsync();
+            if(order == null || analysis.Count < 1)
+            {
+                ErrorMessage = "Order or Analysis(es) not found";
+                return RedirectToAction(nameof(OrdersController.Index),"Orders");
+            }
+            foreach(var item in analysis)
+            {
+               var chargeToAdd = new Charges();
+               chargeToAdd.OrderId = order.Id;
+               chargeToAdd.ContactId = order.ContactId;
+               chargeToAdd.Description = item.AnalysisRequirement.Name;
+               chargeToAdd.ItemCount = item.NumberAnalyzed.Value;
+
+               if(order.PaymentMethod == PaymentTypes.IOC.GetDisplayName())
+               {
+                   chargeToAdd.Cost = item.AnalysisRequirement.InternalCost;
+               } else
+               {
+                  chargeToAdd.Cost = item.AnalysisRequirement.ExternalCost;
+               }
+               if(ModelState.IsValid)
+               {
+                  _dbContext.Add(chargeToAdd);                     
+               } else 
+               {
+                  ErrorMessage = "Something went wrong";
+                  return RedirectToAction(nameof(OrdersController.Index),"Orders");
+               }
+            }
+            await _dbContext.SaveChangesAsync();
+            Message = "Add charges added";
+            return RedirectToAction(nameof(OrdersController.Details),"Orders", new {  id = id});
+         }
+
+    }
+}
---
+++ Models/Charges.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using SIFCore.Helpers;
+
+namespace SIFCore.Models
+{
+    public class Charges
+    {
+        public Charges()
+        {   
+            Paid = false;
+            DateCharged =  DateTime.Now.Date;
+        }       
+
+        public int Id { get; set; }
+
+        [ForeignKey("ContactId")]
+        public  Contacts Contact { get; set; }
+
+        [Required]
+        public int ContactId { get; set; }
+
+        public int OrderId { get; set; }
+
+        [ForeignKey("OrderId")]
+        public Orders Order { get; set; }
+
+        public string Description { get; set; }
+
+        public int ItemCount { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:n2}", ApplyFormatInEditMode = true)]
+        public  decimal Cost  { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public decimal LineTotal { get; set; }
+        
+
+        [DataType(DataType.Date)]
+        [Display(Name="Date Charged")]
+        public  DateTime  DateCharged { get; set; }
+
+        public bool Paid { get; set; }
+
+       
+    }     
+    
+}
